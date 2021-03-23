59fa2a1d844eaab9a9dd89fb2a0ed0db62078b97
SeppeG <geuensseppe@gmail.com>
2021-03-23 21:04:49 +0100

    fixed error in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\ArticlesController;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
