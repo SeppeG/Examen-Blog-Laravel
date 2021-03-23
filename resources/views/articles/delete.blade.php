10db844b4b7c53e7f86069b643706f5758a92be3
SeppeG <geuensseppe@gmail.com>
2021-03-23 22:43:14 +0100

    Update delete.blade.php

--- resources/views/articles/delete.blade.php
+++ resources/views/articles/delete.blade.php
@@ -3,8 +3,12 @@
 @section('content')
 <div id="wrapper">
     <div id="page" class="container">
+        @can('edit-article', $article)
         <h2></h2> article named "{{$article->title}}" has been removed.
         <form action="/articles">@csrf<button class="button is-link" type="submit">Back to articles</button></form>
+        @else
+        <h2 style="color: rgb(138, 34, 34)">You are not authorized to delete this article.</h2>
+        @endcan
     </div>
 </div>
 @endsection
