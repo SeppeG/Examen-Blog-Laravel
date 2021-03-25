63006c48fb14e15b2aca0e3d91078a51ea8c08ce
SeppeG <geuensseppe@gmail.com>
2021-03-25 14:12:38 +0100

    favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,6 +13,7 @@ Released   : 20140225
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
+    <link rel="shortcut icon" href="favicon.png" type="image/png">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title></title>
     <meta name="keywords" content="" />
